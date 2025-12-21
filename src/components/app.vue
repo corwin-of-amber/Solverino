6809e20bf58f490f13067c4fd64e13e41d9e185b
Shachar Itzhaky <corwin.amber@gmail.com>
2025-12-21 22:46:47 +0200

    [feature] Dragging mode.

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,17 +1,29 @@
 <template>
     <Board :data="board" :size="[12, 5]"
         @cell:select="revealPiece($event.selection)"></Board>
-    <div class="pieces">
+    <div class="pieces" :class="{[`phase-${phase}`]: true}">
         <div v-for="penta in pentas" class="card" 
                 :class="{selected: selected.includes(penta)}"
-                @click="toggleSelect(penta)">
+                @click="phase == 'select' && toggleSelect(penta)"
+                @pointerdown="phase == 'game' && dragPiece($event, penta)">
             <Board :data="[[penta, [0,0]]]" selectMode="none"></Board>
         </div>
     </div>
     <button @click="solve">Solve</button>
+
+    <Draggable ref="dragged" class="drag-piece card" v-slot="{ position }"
+            :prevent-default="true" :class="{hidden: !dragging.penta}">
+        <Board v-if="dragging.penta" :data="[[dragging.penta, [0,0]]]" selectMode="none"
+            :style="dragStyle()"></Board>
+        {{ (dragging.position = position) && undefined }}
+    </Draggable>
 </template>
 
 <style lang="scss">
+.drag-piece {
+    position: fixed;
+    &.hidden { display: none; }
+}
 div.pieces {
     display: grid;
     grid-auto-flow: column;
@@ -31,13 +43,25 @@ div.pieces {
                     outline-color 0.5s;
         top: 0px;
         outline: 1px solid transparent;
-        &.selected {
-            top: -7px;
-            outline: 1px solid #800;
-            background-color: #fcda;
-        }
     }
+}
 
+div.pieces.phase-select {
+    div.card.selected {
+        top: -7px;
+        outline: 1px solid #800;
+        background-color: #fcda;
+    }
+}
+
+div.pieces.phase-game {
+    div.card:not(.selected) {
+        opacity: 0.25;
+        pointer-events: none;
+    }
+}
+
+div.card {
     .tabular {
         background: none;
         filter: drop-shadow(0px 0px 2px #0006);
@@ -49,18 +73,18 @@ div.pieces {
         }
     }
 }
-
 </style>
 
 <script lang="ts">
 import _ from 'lodash';
-import { Vue, toNative, Component } from 'vue-facing-decorator';
+import { Vue, toNative, Component, Ref } from 'vue-facing-decorator';
+import { UseDraggable } from '@vueuse/components';
 import { XY } from '../infra/geom2d';
 import Board, { BoardData, Piece } from './board.vue';
 
 
 @Component({
-    components: { Board }
+    components: { Board, Draggable: UseDraggable }
 })
 class IApp extends Vue {
     board: BoardData = (JSON.parse(MOCK_JSON) as [XY[], XY][]).map(
@@ -68,6 +92,7 @@ class IApp extends Vue {
         );
     pentas: Piece[]
 
+    phase: 'select' | 'game' = 'game'
     selected: Piece[] = []
 
     solution: BoardData = []
@@ -81,6 +106,14 @@ class IApp extends Vue {
         await this.loadPentas();
         /** @todo load last set from local storage */
         this.selected = [2,5,10].map(i => this.pentas[i]);
+
+        document.addEventListener('keydown', ev => {
+            switch (ev.code) {
+                case 'ShiftLeft': 
+                    this.dragging.rotate += 90; break;
+
+            }
+        });
     }
 
     async loadPentas() {
@@ -126,6 +159,44 @@ class IApp extends Vue {
             this.solution.filter(([{blocks}, [x, y]]) =>
                 blocks.some(([dx, dy]) =>
                     x + dx == selection.col - 1 && y + dy == selection.row - 1));
+    }
+    
+    // -------------
+    // Dragging Part
+    // -------------
+
+    @Ref dragged: {$el: HTMLElement}
+    dragging: {
+        penta?: Piece,
+        grab?: {x: number, y: number},
+        position?: {x: number, y: number},
+        rotate: 0 | 90 | 180 | 270
+    } = {rotate: 0}
+
+    dragPiece(ev: PointerEvent, penta: Piece) {
+        this.dragging.penta = penta;
+        let el = (ev.target as HTMLElement).closest('.tabular');
+        console.log(ev.target);
+        let r = el.getBoundingClientRect();
+        this.dragging.position.x = r.left;
+        this.dragging.position.y = r.top;
+        this.dragging.grab = {x: ev.clientX - r.left, y: ev.clientY - r.top};
+        requestAnimationFrame(() => this.dragged.$el.dispatchEvent(ev));
+        ev.preventDefault();
+    }
+
+    dragRotate(by: 90 | -90) {
+        let d = this.dragging.rotate + by;
+        if (d > 360) d -= 360;
+        if (d < 0) d += 360;
+        this.dragging.rotate = d as 0 | 90 | 180 | 270;
+    }
+
+    dragStyle() {
+        return {
+            transform: `rotate(${this.dragging.rotate}deg) scale(2)`,
+            'transform-origin': (pt => pt && `${pt.x}px ${pt.y}px`)(this.dragging.grab)
+        };
     }
 }
 
